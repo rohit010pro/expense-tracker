2bcee0ce3ae53cd5e34e6ff97d212b505424d7c7
rohit010pro <rohit777kanojiya@gmail.com>
2024-04-09 10:20:30 +0530

    updated index.json

--- server/index.js
+++ server/index.js
@@ -19,10 +19,10 @@ app.use(cors({
     credentials: true
 }))
 
+app.use(bodyparser.json({limit: "16kb"}));
+
 // parse request to body-parser
 app.use(bodyparser.urlencoded({ extended : true, limit: "16kb"}))
-
-app.use(express.json({limit: "16kb"}));
 
 app.use('/uploads', express.static('uploads'))
 
